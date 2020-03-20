d2025b10e4a155a7200e77fc96eb051dca1c0054
alex5955 <43375012+alex5955@users.noreply.github.com>
2020-03-20 11:08:56 +0100

    Class and movement sequence

--- Drone.py
+++ Drone.py
@@ -1,14 +1,49 @@
 import socket
-import sys
 import time
 
-
 class Drone(object):
-    """description of class"""
 
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.address = (self.ip,self.port)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sendCommand("command")    
+
+    def sendCommand(self, command):
+        self.sock.sendto(command.encode(), (self.address))
+        print("Sent command: " + command)
 
     def takeOff(self):
-        print("takeOff")
-        #add the takeoff code
+        self.sendCommand("takeoff")
 
+    def land(self):
+        self.sendCommand("land")
+
+    def battery(self):
+        self.sendCommand("battery?")
+
+    def end(self):
+        print("close")
+        self.sock.close()
+        
+    def forward(self, amount):
+        self.sendCommand("forward " + amount)
+
+    def back(self, amount):
+        self.sendCommand("back " + amount)
+
+    def rotateCW(self,x):
+        self.sendCommand("cw" + x)
+
+    def rotateCCW(self,x):
+        self.sendCommand("ccw" + x)
+
+    def printinfo(self):
+        print("Hallo Drone at : IP:" + self.ip + " Port: " + self.port)
+
+    def left(self, amount):
+        self.sendCommand("left " + amount)
     
+    def right(self, amount):
+        self.sendCommand("right " + amount)
--- Main.py
+++ Main.py
@@ -1,2 +1,65 @@
+import drone
+import time
 
-#here you should interact with the drone
+drone1 = drone.Drone('192.168.10.1',8889)
+
+#Info printed
+drone1.battery()
+drone1.printinfo()
+
+#Takeoff Drone
+drone1.takeOff()
+time.sleep(2)
+
+#Square routine
+
+#Moving forward + rotating 90 degrees
+drone1.forward(25)
+time.sleep(2)
+drone1.rotateCW("90")
+time.sleep(2)
+
+#
+drone1.forward(25)
+time.sleep(2)
+drone1.rotateCW("90")
+time.sleep(2)
+
+#
+drone1.forward(25)
+time.sleep(2)
+drone1.rotateCW("90")
+time.sleep(2)
+
+#
+drone1.forward(25)
+time.sleep(2)
+drone1.rotateCW("90")
+time.sleep(2)
+
+# cross movement
+drone1.forward(25)
+time.sleep(2)
+drone1.back(25)
+time.sleep(2)
+drone1.left(25)
+time.sleep(2)
+drone1.right(25)
+time.sleep(2)
+drone1.right(25)
+time.sleep(2)
+drone1.left(25)
+time.sleep(2)
+drone1.back(25)
+time.sleep(2)
+drone1.forward(25)
+time.sleep(2)
+
+#Land
+drone1.battery()
+drone1.printinfo()
+drone1.land()
+
+#end program
+drone1.end()
+
